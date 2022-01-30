a1daec2a9958e27a357386e24b5895e180eeeb20
a414242 <a414242@CNTSNWCSFNBK3.vcn.ds.volvo.net>
2022-01-30 15:32:01 +0800

    set the value of test from env

--- src/main/java/com/example/demo2/ApiController.java
+++ src/main/java/com/example/demo2/ApiController.java
@@ -2,6 +2,7 @@ package com.example.demo2;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,9 @@ import lombok.Data;
 @RequestMapping("/test")
 public class ApiController {
 
+    @Value("${test.add.env.via.openshift}")
+    private String test;
+    
 //	@PostMapping("/post")
 	@GetMapping(name = "get1",value = "get1",produces = {MediaType.APPLICATION_JSON_VALUE})
 	@ApiOperation("get test without parameters")
@@ -24,7 +28,8 @@ public class ApiController {
 		logger.info("just test");
 		
 		User u = new User();
-		u.setName("abctest-demo3");
+		u.setName("abctest-demo3"+" " + test);
+		
 		return u;
 	}
 	
